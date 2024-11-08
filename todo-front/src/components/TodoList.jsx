e82a7f0d14464654ecf7f00e654185278751fedb
Chowonchan <cwc2264@gmail.com>
2024-11-08 14:05:16 +0900

    2024 11 08 todo-front

---
+++ todo-front/src/components/TodoList.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useContext } from 'react';
+
+const TodoListComponent = () => {
+
+  return(
+    <>
+      <h1>000의 TodoLsit</h1>
+    </>
+  );
+}
+
+export default TodoListComponent;
